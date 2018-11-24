e787c22fc9e93b3fc439e9a1f012e7cd967eca88
wahyu alfarisi <wahyualfarisi@gmail.com>
2018-11-24 11:22:13 +0700

    adding get by id API

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 var express    = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo}     = require('./models/todo');
 var {User}     = require('./models/users');
+
 
 var app = express();
 
@@ -27,6 +29,24 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.send(e)
   })
+})
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+      return res.status(400).send()
+    }
+
+    Todo.findById(id).then( (todo) => {
+      if(!todo){
+        return res.status(404).send();
+      }
+      res.send({todo});
+    }, (e) => {
+      res.status(400).send()
+    })
+
 })
 
 
