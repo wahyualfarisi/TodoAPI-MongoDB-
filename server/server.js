513818d7c23d5ce2489d364f3767519ccfc4b770
wahyu alfarisi <wahyualfarisi@gmail.com>
2018-11-25 18:50:50 +0700

    add patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-var express    = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo}     = require('./models/todo');
-var {User}     = require('./models/users');
+const _          = require('lodash');
+const express    = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./db/mongoose');
+const {Todo}     = require('./models/todo');
+const {User}     = require('./models/users');
 
 
 var app = express();
@@ -12,6 +14,7 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json() );
 
+//------------------------------------------------------------------------------
 app.post('/todos', (req, res) => {
   var insert = new Todo({
     text: req.body.text
@@ -23,6 +26,7 @@ app.post('/todos', (req, res) => {
     res.status(400).send(e)
   })
 })
+//------------------------------------------------------------------------------
 
 app.get('/todos', (req, res) => {
   Todo.find().then( (todos) => {
@@ -31,6 +35,8 @@ app.get('/todos', (req, res) => {
     res.send(e)
   })
 })
+
+//------------------------------------------------------------------------------
 
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
@@ -49,6 +55,8 @@ app.get('/todos/:id', (req, res) => {
     })
 })
 
+//------------------------------------------------------------------------------
+
 app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
 
@@ -65,8 +73,40 @@ app.delete('/todos/:id', (req, res) => {
   }, (e) => {
     res.status(400).send()
   })
+})
+
+//------------------------------------------------------------------------------
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  var body = _.pick(req.body, ['text', 'completed'] );
+
+  if(!ObjectID.isValid(id) ){
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed ){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed   = false;
+    body.completedAt = null;
+  }
+
+  Todo.findOneAndUpdate(id, {$set: body}, {new: true}).then( (todo) => {
+    if(!todo){
+      res.status(404).send();
+    }
+
+    res.send({todo});
+
+  }).catch( (e) => {
+    res.status(400).send()
+  } )
 
 })
+
+
 
 
 
