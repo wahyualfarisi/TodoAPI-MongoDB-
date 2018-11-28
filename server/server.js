41796d487742ccd2bcd62646b9fef74966ab341c
wahyu alfarisi <wahyualfarisi@gmail.com>
2018-11-28 19:41:22 +0700

    add middleware

--- server/server.js
+++ server/server.js
@@ -119,8 +119,30 @@ app.post('/users', (req, res) => {
   }).catch( (e) => {
     res.status(400).send(e)
   })
-
 })
+
+
+var authenticate = (req, res, next) => {
+  var token = req.header('x-auth');
+
+  User.findByToken(token).then( (user) => {
+    if(!user){
+      return Promise.reject();
+    }
+
+    req.user  = user;
+    req.token = token;
+    next();
+  }).catch( (e) => {
+    res.status(401).send();
+  })
+
+}
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user)
+});
 
 
 
